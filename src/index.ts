471108baa3f1a48e64782598282b18f7f366047a
davidlainesv <david.dev@outlook.com>
2023-04-11 10:41:08 -0600

    change port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import activities from './endpoints/activities/router'
 import students from './endpoints/students/router'
 
-const PORT = 3000
+const PORT = 8080
 
 const app = express()
 
